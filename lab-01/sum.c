fcfc3b147d4dbdc7abc676157a3ae02da2557a4d
santiago <santiagoperayre@gmail>
2024-03-15 07:40:59 -0300

    Agregados correcciones a hola.c y el ejercicio de sum.c

--- lab-01/sum.c
+++ lab-01/sum.c
@@ -4,7 +4,14 @@
 int main(int argc, char *argv[])
 {
     // Agregar código aquí.
-
+	int sum = 0;
+	int i;
+	
+	for (i = 1; i < 4; i++) {
+		sum += atoi(argv[i]);
+	}
+	
+	printf("La suma es: %d\n", sum);
     // Termina la ejecución del programa.
     exit(EXIT_SUCCESS);
 }
