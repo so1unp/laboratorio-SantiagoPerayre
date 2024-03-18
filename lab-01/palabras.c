4778282d05f2e415db40a5add2bfa5461a222b47
santiago <santiagoperayre@gmail.com>
2024-03-18 18:43:11 -0300

    ejercicio palabras

--- lab-01/palabras.c
+++ lab-01/palabras.c
@@ -4,7 +4,19 @@
 int main(int argc, char *argv[])
 {
     // Agregar código aquí.
+    int c;
 
+    while(1) {
+        while((c = getchar()) != ' ' && c != '\t' != EOF) {
+            putchar(c);
+        }
+
+        printf("\n");
+
+        if(c == EOF) {
+            break;
+        }
+    }
     // Termina la ejecución del programa.
     exit(EXIT_SUCCESS);
 }
