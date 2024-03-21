d858901249ce76f53c1efb2074e4146413152169
santiago <santiagoperayre@gmail.com>
2024-03-21 17:10:38 -0300

    Ej histograma

--- lab-01/histograma.c
+++ lab-01/histograma.c
@@ -1,10 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
-    // Agregar código aquí.
+#define maxLetras 10
 
-    // Termina la ejecución del programa.
+int main(int argc, char *argv[]) {
+    
+    // Declaracion de variables
+    int frecuencia[maxLetras] = {0}; // Inicializar frecuencia con 0s
+    int i;
+    int c;
+    int letras;
+    int maxPalabras = 0;
+
+    // Recopilar informacion introducida con la funcion getchar() y 
+    // guardado en array para dibujar el histograma
+    while(1) {
+        letras = 0;
+        
+        while((c = getchar()) != ' ' && c != '\n' && c != '\t' && c != EOF) {
+            letras += 1;
+        }
+        
+        if(letras != 0) {
+            frecuencia[letras] += 1;
+        }
+
+        if(c == EOF) {
+            break;
+        }
+
+        if(letras > maxPalabras) {
+            maxPalabras = letras;
+        }
+    }
+    
+    // Imprimir histograma
+    for (i = 1; i <= maxPalabras; i++) {
+        printf("%d: ", i);
+        for (int j = 0; j < frecuencia[i]; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
     exit(EXIT_SUCCESS);
 }
